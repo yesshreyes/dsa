cecb7752dee0759fa8cdedaeedbadd62d5bd967b
Shreyas Deshmukh <shreyesdeshmukh03@gmail.com>
2025-06-24 11:34:28 +0530

    Array - kthMinMax

---
+++ src/arrays/kthMinMax.java
@@ -0,0 +1,18 @@
+package arrays;
+
+import java.util.Arrays;
+
+class KthMinMax {
+    static void findKthMinMax(int[] arr, int k) {
+        Arrays.sort(arr);
+        int kthMin = arr[k - 1];
+        int kthMax = arr[arr.length - k];
+        System.out.println(k + "th Minimum element: " + kthMin);
+        System.out.println(k + "th Maximum element: " + kthMax);
+    }
+    public static void main(String[] args) {
+        int[] arr = {7, 10, 4, 3, 20, 15};
+        int k = 3;
+        findKthMinMax(arr, k);
+    }
+}
