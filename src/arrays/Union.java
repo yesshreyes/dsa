4e4b711b5a796516e99715e2c699d66cc355a105
Shreyas Deshmukh <shreyesdeshmukh03@gmail.com>
2025-06-24 20:17:10 +0530

    Array - Union

---
+++ src/arrays/Union.java
@@ -0,0 +1,21 @@
+package arrays;
+
+import java.util.HashSet;
+
+//Union using HashSet
+public class Union {
+    public static int doUnion(int[] a, int n, int[] b, int m) {
+        HashSet<Integer> s = new HashSet<>();
+        for (int i = 0; i < n; i++)
+            s.add(a[i]);
+        for (int i = 0; i < m; i++)
+            s.add(b[i]);
+        return s.size();
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {1, 2, 3, 6, 7};
+        System.out.println(doUnion(a, a.length, b, b.length)); // Output: 7
+    }
+}
