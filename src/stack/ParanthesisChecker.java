f6084a526d8b4bf074ecffd1d50d652549874cd5
Shreyas Deshmukh <shreyesdeshmukh03@gmail.com>
2025-07-11 11:45:00 +0530

    Stack - ParenthesisChecker

---
+++ src/stack/ParanthesisChecker.java
@@ -0,0 +1,29 @@
+package stack;
+import java.util.Stack;
+
+public class ParanthesisChecker {
+
+    static class Solution {
+        static boolean isBalanced(String s) {
+            Stack<Character> stack = new Stack<>();
+
+            for (char ch : s.toCharArray()) {
+                if (ch == '(' || ch == '{' || ch == '[') {
+                    stack.push(ch);
+                }
+                else {
+                    if (stack.isEmpty()) return false;
+
+                    char top = stack.pop();
+                    if ((ch == ')' && top != '(') ||
+                            (ch == '}' && top != '{') ||
+                            (ch == ']' && top != '[')) {
+                        return false;
+                    }
+                }
+            }
+            return stack.isEmpty();
+        }
+    }
+
+}
--- src/stack/Reverse.java
+++ src/stack/Reverse.java
@@ -19,5 +19,4 @@ public class Reverse {
             return reversed.toString();
         }
     }
-
 }
